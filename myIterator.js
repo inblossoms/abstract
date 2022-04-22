7401b098ea3a08e9f7fcfe7c3fdccd019ad2ba5e
荞与 <87680384+MEIYA-yl@users.noreply.github.com>
2022-04-22 22:39:17 +0800

    Update myIterator.js
    类的方式模拟迭代器

--- myIterator.js
+++ myIterator.js
@@ -20,3 +20,33 @@ console.log(iterator.next());
 console.log(iterator.next());
 console.log(iterator.next());
 console.log(iterator.next());
+
+
+class SimpleClass {
+  constructor(data) {
+    this.data = data
+  }
+
+  [Symbol.iterator]() {
+    // Use a new index for each iterator. This makes multiple
+    // iterations over the iterable safe for non-trivial cases,
+    // such as use of break or nested looping over the same iterable.
+    let index = 0;
+
+    return {
+      next: () => {
+        if (index < this.data.length) {
+          return {value: this.data[index++], done: false}
+        } else {
+          return {done: true}
+        }
+      }
+    }
+  }
+}
+
+const simple = new SimpleClass([1,2,3,4,5])
+
+for (const val of simple) {
+  console.log(val)   //'1' '2' '3' '4' '5'
+}
