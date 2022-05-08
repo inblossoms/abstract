3f406ee40e4a3428ab0ad94ea9207031dd0657dd
MEIYA-yl <2648297163@qq.com>
2022-05-08 22:40:47 +0800

    队列

---
+++ JS进阶/内存与数据结构/dist/Queue.js
@@ -0,0 +1,56 @@
+"use strict";
+/** @format */
+let queue = {
+    0: 'a',
+    1: 'b',
+    2: 'c',
+    3: 'd',
+};
+//  队列对象的基本代码结构如下：
+class Queue {
+    constructor() {
+        this.length = 0;
+        this.queue = {};
+    }
+    // 从队列尾巴入队
+    push(node) {
+        this.queue[this.length] = node;
+        this.length++;
+        return this.queue;
+    }
+    // 从队列头部出队
+    shift() {
+        const target = this.queue[0];
+        for (let i = 0; i < this.length - 1; i++) {
+            this.queue[i] = this.queue[i + 1];
+        }
+        delete this.queue[this.length - 1]; // 出队后，所有值前移 所以末位值需要去掉
+        this.length--;
+        return target;
+    }
+    // 特殊情况的插队处理，在 idx 位置前插入
+    insert(idx, node) {
+        this.length++;
+        for (let k = this.length - 1; k > idx; k--) {
+            this.queue[k] = this.queue[k - 1];
+        }
+        this.queue[idx] = node;
+        return this.queue;
+    }
+    // 特殊情况的离队处理，可以在队列的任意位置离队
+    out(idx) {
+        const target = this.queue[idx];
+        this.length--;
+        for (let k = idx; k < this.length; k++) {
+            this.queue[k] = this.queue[k + 1];
+        }
+        delete this.queue[this.length - 1];
+        this.length--;
+        return target;
+    }
+    // 清空队列
+    clear() {
+        this.length = 0;
+        this.queue = {};
+    }
+}
